bdbe36e3c00d5773c7156110b39150fb80eebdf1
ondrvaakkk <snoplik@gmail.com>
2016-07-18 03:40:55 +0200

    Update newbot.js

--- newbot.js
+++ newbot.js
@@ -48,7 +48,7 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: "Worldbot",
       language: "czech",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/cs.json",
+      chatLink: "https://rawgit.com/ondrvaakkk/plugdj_supercreep/master/cs.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
